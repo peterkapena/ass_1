5a457e4a794ff97b70018a6dd5670fa3a066800e
Peter Lumumba <Peter@devman3.com>
2022-03-14 22:43:26 +0200

    good

--- Q1.js
+++ Q1.js
@@ -10,7 +10,7 @@ webgl.clearColor(0, 0, 0, 1);
 
 webgl.clear(webgl.COLOR_BUFFER_BIT);
 
-const values = [0.5, 0.5, 1.0, 0.5, 1.0, 1.0];
+const values = [-0.25, -0.25, -.25, -0.125, -.125, -.25];
 const vertices = new Float32Array(values);
 
 const buffer = webgl.createBuffer();
