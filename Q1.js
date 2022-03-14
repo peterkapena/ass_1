cc877076cad802c249b178dc70e50fbb7bbe19af
Peter Lumumba <Peter@devman3.com>
2022-03-14 22:40:19 +0200

    initialized

---
+++ Q1.js
@@ -0,0 +1,50 @@
+const canvas = document.querySelector("canvas");
+
+const webgl = canvas.getContext("webgl");
+
+if (!webgl) {
+  throw new Error("WebGL not available/supported");
+}
+
+webgl.clearColor(0, 0, 0, 1);
+
+webgl.clear(webgl.COLOR_BUFFER_BIT);
+
+const values = [0.5, 0.5, 1.0, 0.5, 1.0, 1.0];
+const vertices = new Float32Array(values);
+
+const buffer = webgl.createBuffer();
+webgl.bindBuffer(webgl.ARRAY_BUFFER, buffer);
+webgl.bufferData(webgl.ARRAY_BUFFER, vertices, webgl.STATIC_DRAW);
+
+const vertexShader = webgl.createShader(webgl.VERTEX_SHADER);
+webgl.shaderSource(
+  vertexShader,
+  `attribute vec2 pos;
+    void main(){
+        gl_Position = vec4(pos, 0, 1);
+    }
+`
+);
+webgl.compileShader(vertexShader);
+
+const fragmentShader = webgl.createShader(webgl.FRAGMENT_SHADER);
+webgl.shaderSource(
+  fragmentShader,
+  `void main(){
+      gl_FragColor = vec4(1.0, 0, 0, 1.0);
+  }
+`
+);
+webgl.compileShader(fragmentShader);
+
+const program = webgl.createProgram();
+webgl.attachShader(program, vertexShader);
+webgl.attachShader(program, fragmentShader);
+webgl.linkProgram(program);
+
+const positionLocation = webgl.getAttribLocation(program, "pos");
+webgl.enableVertexAttribArray(positionLocation);
+webgl.vertexAttribPointer(positionLocation, 2, webgl.FLOAT, false, 0, 0);
+webgl.useProgram(program);
+webgl.drawArrays(webgl.TRIANGLES, 0, vertices.length / 2);
